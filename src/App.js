f19bc5e4c1ddf336ef0948dd9461608215496217
kalpesh-gavane <kalpeshgavane@gmail.com>
2022-10-16 22:38:25 +0530

    checking 3

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = (type) => {
           <Route exact path='/cart' element={<CartContainer />} />
           <Route exact path='/checkout' element={<Checkout />} />
         </Routes> 
+        
       </BrowserRouter>
     </div>
   );
